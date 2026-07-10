05890f1efc4f1f7e7a3a1bedcf20f8f1305dcf71
Ellis <ellis.gove@students.plymouth.ac.uk>
2026-07-11 00:04:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,7 @@ from models import Subscription
 app = config.connex_app
 app.add_api(config.basedir / "swagger.yaml")
 
+#I have not included a route, go to api/subscription to see database working
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
